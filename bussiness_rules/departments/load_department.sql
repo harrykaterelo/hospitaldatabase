2dacd360a1220b9c0b361dd7f65fd1f96df58fb7
mac1727 <giannismacmillan17@gmail.com>
2026-05-09 20:13:26 +0300

    first trigger for department

--- bussiness_rules/departments/load_department.sql
+++ bussiness_rules/departments/load_department.sql
@@ -1,28 +1,20 @@
 CREATE TABLE tmima (
-    onoma           VARCHAR(100)    NOT NULL,
+    onoma           VARCHAR(100)    NOT NULL UNIQUE,
     perigrafi       TEXT            NULL,
     arithmos_klinon SMALLINT        NOT NULL CHECK (arithmos_klinon >= 0),
-    rofos_ktiriou   VARCHAR(50)     NOT NULL,
-    amka_dieftinti  CHAR(11)        NULL,  -- FK προς iatros (circular → add after)
+    orofos_ktiriou   VARCHAR(50)     NOT NULL,
+    amka_dieftinti  CHAR(11)        NULL, 
     tmima_id        INT             AUTO_INCREMENT,
     PRIMARY KEY (tmima_id),
-    UNIQUE (onoma)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
  
--- ============================================================
--- 6. ΙΑΤΡΟΣ  (ISA from proswpiko)
--- ============================================================
 
- 
--- Τώρα προσθέτουμε FK tmima → iatros (circular χρειάζεται ALTER)
 ALTER TABLE tmima
     ADD CONSTRAINT fk_tmima_dieftintis
     FOREIGN KEY (amka_dieftinti) REFERENCES iatros(amka)
     ON DELETE SET NULL ON UPDATE CASCADE;
  
--- ============================================================
--- 7. ΙΑΤΡΟΣ–ΤΜΗΜΑ  (M:N, ιατρός μπορεί σε πολλά τμήματα)
--- ============================================================
+
 CREATE TABLE proswpiko_anikei_se_tmima (
     amka_proswpikou CHAR(11) NOT NULL,
     tmima_id        INT      NOT NULL,
@@ -38,7 +30,7 @@ CREATE TABLE proswpiko_anikei_se_tmima (
         REFERENCES tmima(tmima_id)
         ON DELETE CASCADE
         ON UPDATE CASCADE
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;;
 
 CREATE TABLE klini (   
     tmima_id        INT             NOT NULL,
