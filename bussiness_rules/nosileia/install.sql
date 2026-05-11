41201706e1ee5b998c9319a975df51c67c5b91e6
mac1727 <giannismacmillan17@gmail.com>
2026-05-11 20:00:57 +0300

    dd

--- bussiness_rules/nosileia/install.sql
+++ bussiness_rules/nosileia/install.sql
@@ -27,7 +27,7 @@ CREATE TABLE ektakti_epafi (
 CREATE TABLE ken (
     kod_ken         VARCHAR(20)     NOT NULL,
     vasiko_kostos   DECIMAL(10,2)   NOT NULL CHECK (vasiko_kostos >= 0),
-    mdn             DECIMAL(5,1)    NOT NULL CHECK (mdn > 0),
+    mdn             SMALLINT        NOT NULL CHECK (mdn > 0),
     imer_xrewsi     DECIMAL(8,2)    NOT NULL CHECK (imer_xrewsi >= 0),
     PRIMARY KEY (kod_ken)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
