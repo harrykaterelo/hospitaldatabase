fb0b12869c0308ca39d1a08095cf0ab66b300a39
mac1727 <giannismacmillan17@gmail.com>
2026-05-18 22:36:05 +0300

    new queries and indexes

---
+++ indexes.sql
@@ -0,0 +1,19 @@
+-- ============================================================
+-- Indexes για τα queries 1-5
+-- ============================================================
+
+-- Query 1: Έσοδα ανά τμήμα και ανά έτος με ανάλυση ανά ΚΕΝ
+CREATE INDEX idx_nosileia_tmima_eisodos ON nosileia(tmima_id, imerominia_eisodou);
+
+-- Query 2: Ιατροί ανά ειδικότητα με εφημερίες και επεμβάσεις
+CREATE INDEX idx_eidikotita ON iatros(eidikotita);
+CREATE INDEX idx_efimeria_amka_imerominia ON efimeria_proswpiko(amka_proswpiko, imerominia);
+
+-- Query 3: Ασθενείς με 3+ νοσηλείες στο ίδιο τμήμα
+CREATE INDEX idx_nosileia_asthenis_tmima ON nosileia(amka_astheni, tmima_id);
+
+-- Query 4: Μέσος όρος αξιολογήσεων για συγκεκριμένο ιατρό
+-- (χρησιμοποιεί ήδη υπάρχοντα FK indexes: exetasi.amka_iatrou, iatrikipraxi.amka_kyriou_xeirourgou)
+
+-- Query 5: Νέοι ιατροί (ηλικία < 35) με τις περισσότερες χειρουργικές επεμβάσεις
+CREATE INDEX idx_ilikia ON anthropos(ilikia);
---
+++ queries/2.sql
@@ -0,0 +1,29 @@
+DROP PROCEDURE IF EXISTS query_2;
+
+DELIMITER //
+CREATE PROCEDURE query_2(IN p_eidikotita VARCHAR(80))
+BEGIN
+    WITH efimeries AS (
+        SELECT DISTINCT amka_proswpiko
+        FROM efimeria_proswpiko
+        WHERE YEAR(imerominia) = YEAR(CURDATE())
+    ),
+    epemvaseis AS (
+        SELECT
+            amka_kyriou_xeirourgou,
+            COUNT(*) AS arithmos_epemvaseon
+        FROM iatrikipraxi
+        GROUP BY amka_kyriou_xeirourgou
+    )
+    SELECT
+        i.amka,
+        i.eidikotita,
+        IF(ef.amka_proswpiko IS NOT NULL, 'Ναι', 'Όχι') AS eixe_efimeria,
+        COALESCE(ep.arithmos_epemvaseon, 0)             AS arithmos_epemvaseon
+    FROM iatros i
+    LEFT JOIN efimeries ef  ON ef.amka_proswpiko = i.amka
+    LEFT JOIN epemvaseis ep ON ep.amka_kyriou_xeirourgou = i.amka
+    WHERE i.eidikotita = p_eidikotita
+    ORDER BY i.amka;
+END //
+DELIMITER ;
