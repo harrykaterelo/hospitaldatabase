3b1c449e7f4a880d41e0e478e38e08b01ca4e308
haris <katerelosharry@gmail.com>
2026-05-19 20:02:22 +0300

    added queries 11 and 8

--- queries/2.sql
+++ queries/2.sql
@@ -13,14 +13,15 @@ BEGIN
             amka_kyriou_xeirourgou,
             COUNT(*) AS arithmos_epemvaseon
         FROM iatrikipraxi
-        WHERE katigoria = 'Χειρουργική'
+        
         GROUP BY amka_kyriou_xeirourgou
     )
     SELECT
         i.amka,
         i.eidikotita,
         IF(ef.amka_proswpiko IS NOT NULL, 'Ναι', 'Όχι') AS eixe_efimeria,
-        COALESCE(ep.arithmos_epemvaseon, 0)             AS arithmos_epemvaseon
+        COALESCE(ep.arithmos_epemvaseon, 0)             AS arithmos_epemvaseon,
+    SUM(COALESCE(ep.arithmos_epemvaseon, 0)) OVER () AS synolo_epemvaseon
     FROM iatros i
     LEFT JOIN efimeries ef  ON ef.amka_proswpiko = i.amka
     LEFT JOIN epemvaseis ep ON ep.amka_kyriou_xeirourgou = i.amka
